8c946c9607babdab7dfd91f1f35143bd293bd00e
Maxim-Davydov <116034877+Maxim-Davydov@users.noreply.github.com>
2022-12-05 12:02:03 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class HelloWorld
+{
+    static void Main(string[] args)
+    {
+        {
+            Console.WriteLine("Программа для нахождения площади и объёма прямоугольного праллепипеда по его сторонам");
+            Console.Write("Введите значение буквы a "); int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите значение буквы b "); int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите значение буквы c "); int c = Convert.ToInt32(Console.ReadLine());
+            int s = b * c;
+            int v = s * a;
+            Console.Write("Площадь прямоугольного параллипипеда равна: " + s + " Объем прямоугольного параллепипеда равна: " + v);
+            Console.ReadKey(true);
+        }
+    }
+}
