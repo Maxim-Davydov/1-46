986431637dd85b2d3ff8a57a52334a3aa68ea1db
Maxim-Davydov <116034877+Maxim-Davydov@users.noreply.github.com>
2023-02-15 13:30:19 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,13 +1,13 @@
-﻿using System;
+using System;
 class HelloWorld
 {
     static void Main(string[] args)
     {
         {
             Console.WriteLine("Программа для нахождения площади и объёма прямоугольного праллепипеда по его сторонам");
-            Console.Write("Введите значение буквы a "); int a = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Введите значение буквы b "); int b = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Введите значение буквы c "); int c = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите значение стороны длины "); int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите значение стороны ширины "); int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите значение стороны высоты "); int c = Convert.ToInt32(Console.ReadLine());
             int s = b * c;
             int v = s * a;
             Console.Write("Площадь прямоугольного параллипипеда равна: " + s + " Объем прямоугольного параллепипеда равна: " + v);
